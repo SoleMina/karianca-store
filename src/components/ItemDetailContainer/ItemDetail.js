135955cd244f5090ef5247bf8865d994b1d57f25
SoleMina <karina.pradogutierrez@gmail.com>
2021-08-23 23:27:56 -0500

    update item detail

--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -4,15 +4,33 @@ import { ItemCount } from "../ItemCount/ItemCount";
 
 const ItemDetail = ({ item }) => {
   return (
-    <div>
-      <Card className="text-center" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={item.pictureUrl} alt="Producto" />
-        <Card.Body>
-          <Card.Title>{item.title} </Card.Title>
-          <Card.Text>{item.description} </Card.Text>
-          <ItemCount stock={item.stock} initial={1} />
-        </Card.Body>
-      </Card>
+    <div className="container mb-4 hv-85">
+      <div className="row">
+        <div className="col-md-6 d-flex justify-content-center">
+          <Card className="text-center" style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={item.pictureUrl} alt="Producto" />
+            <Card.Body>
+              <Card.Title>{item.title} </Card.Title>
+              <ItemCount stock={item.stock} initial={1} />
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-6">
+          <h1 className="pt-4 text-center">{item.title} </h1>
+          <h5>Stock: {item.stock}</h5>
+          <h5>Category: {item.category}</h5>
+          <h5>Price: {item.price}</h5>
+          <p>
+            <b>Description:</b> {item.description}
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s like Aldus PageMaker including versions of
+            Lorem Ipsum.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -12,30 +12,7 @@ const ItemDetailContainer = ({ match }) => {
     ).then((res) => setItem(res.data[charID]));
   }, [charID]);
 
-  return (
-    <div className="container mb-4 hv-85">
-      <div className="row">
-        <div className="col-md-6 d-flex justify-content-center">
-          <ItemDetail item={item} />
-        </div>
-        <div className="col-md-6">
-          <h1 className="pt-4">Item Detail Container</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s like Aldus PageMaker including versions of
-            Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  return <ItemDetail item={item} />;
 };
 
 export default ItemDetailContainer;
